c997f05a1dfcdb2c9611d5f0be08171588b4b179
Daniel J Blueman <daniel@numascale.com>
2014-04-15 15:10:26 +0800

    Add SPD validity assertion

--- numachip2/spd.c
+++ numachip2/spd.c
@@ -55,5 +55,7 @@ void ddr3_spd_check(const struct ddr3_spd_eeprom *spd)
 	if (spd->crc[0] != crc_lsb || spd->crc[1] != crc_msb)
 		fatal("SPD checksum 0x%02x%02x differs from expected 0x%02x%02x",
 			crc_msb, crc_lsb, spd->crc[1], spd->crc[0]);
+
+	assert(spd->spd_rev >= 0x10);
 }
 
