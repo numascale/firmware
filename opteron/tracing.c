9c771219d215da8571872e1ddc3b4800392531db
Steffen Persvold <sp@numascale.com>
2015-02-12 00:36:27 +0100

    No TCB tracing in the bootloader

--- opteron/tracing.c
+++ opteron/tracing.c
@@ -17,9 +17,6 @@
 
 #include "opteron.h"
 
-#define TCB 1
-#define TCB_SIZE (64 * 8)
-
 void Opteron::tracing_arm(void)
 {
 	xassert((trace_base & 0xffffff) == 0);
@@ -35,19 +32,11 @@ void Opteron::tracing_start(void)
 	write32(TRACE_START, 0);
 	write32(TRACE_STOP, 1 < 31);
 
-	if (TCB) {
-		for (unsigned i = 0; i < TCB_SIZE / 4; i++) {
-			write32(ARRAY_ADDR, 0xa0000000 | i);
-			write32(ARRAY_DATA, 0);
-		}
-	}
-
 	tracing_arm();
 
 	uint32_t val = read32(TRACE_BUF_CTRL);
 	write32(TRACE_BUF_CTRL, val & ~1);
-	write32(TRACE_BUF_CTRL, 1 | (TCB << 1) | (0 << 4) | (1 << 13) | (1 << 20) |
-	  (1 << 20) | (0 << 21) | (0 << 23) | (1 << 25));
+	write32(TRACE_BUF_CTRL, 1 | (0 << 1) | (0 << 4) | (1 << 13) | (1 << 20) | (0 << 21) | (0 << 23) | (1 << 25));
 	write32(TRACE_STOP, 1 | (1 << 4) | (1 << 8) | (1 << 12) | (1 << 16));
 	write32(TRACE_CAPTURE, (1 << 31) | (1 << 30) | (0x3f << 24) | (0x3f << 16));
 
