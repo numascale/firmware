72470229d7db8a4b71615aa99a41aae09529a2eb
Daniel J Blueman <daniel@numascale.com>
2015-05-22 14:06:17 +0800

    Rate-limit error printing; decode LC5 link errors

--- numachip2/lc5.c
+++ numachip2/lc5.c
@@ -41,24 +41,51 @@ void LC5::check(void)
 {
 	uint64_t val = status();
 
-	if (val & ~(1ULL<<31)) {
-		warning("Fabric link %u on %03x has issues 0x%016" PRIx64, index, numachip.sci, val);
+	if (val & ~(1ULL << 31)) {
+		warning("Fabric LC5 link %u on %03x has issues 0x%016" PRIx64 ":", index, numachip.sci, val);
+
+		if (val & (1 << 26)) printf(" Sequencer received an Ack/Nack that was not in the Retry Buffer\n");
+		if (val & (1 << 25)) printf(" Sequence Retry Buffer overflow\n");
+		if (val & (1 << 24)) printf(" Sequence retries has reached maximum\n");
+		if (val & (1 << 22)) printf(" RS credit underflow\n");
+		if (val & (1 << 21)) printf(" CC credit underflow\n");
+		if (val & (1 << 20)) printf(" MC credit underflow\n");
+		if (val & (1 << 19)) printf(" Cross point buffer overflow\n");
+		if (val & (1 << 17)) printf(" Send Buffer Overflow\n");
+		if (val & (1 << 16)) printf(" Double Bit ECC error detected on output of Send Buffer\n");
+		if (val & (1 << 14)) printf(" Output Buffer arbitration error\n");
+		if (val & (1 << 12)) printf(" Output Buffer RS FIFO overflow\n");
+		if (val & (1 << 10)) printf(" Output Buffer CC FIFO overflow\n");
+		if (val & (1 << 8)) printf(" Output Buffer MC FIFO overflow\n");
+		if (val & (1 << 7)) printf(" Receive Retime Buffer overflow\n");
+		if (val & (1 << 6)) printf(" Received an oversized packet\n");
+		if (val & (1 << 5)) printf(" Receive Buffer received unsupported Output Port from Routing table\n");
+		if (val & (1 << 3)) printf(" Receive Buffer bad parity detected on List Ram data\n");
+		if (val & (1 << 2)) printf(" Receive Buffer received a packet that was larger than maximum supported size\n");
+		if (val & (1 << 1)) printf(" Receive Buffer overflow\n");
+		if (val & (1 << 0)) printf(" Receive Buffer received SOP in middle of a packet\n");
+
 		// ratelimit
 		lib::udelay(1000000);
 	}
 
-	/* Clear errors W1TC */
+	// clear errors W1TC
 	numachip.write32(LINKSTAT + index * SIZE, val);
 	numachip.write32(EVENTSTAT + index * SIZE, val >> 32);
 
-	/* Simple link up/down reporting */
-	if ( link_up && ((val & (1ULL<<31)) == 0)) {
-		warning("Fabric link %u is down!", index);
-		link_up = false;
-	}
-	if (!link_up && ((val & (1ULL<<31)) != 0)) {
-		warning("Fabric link %u is up!", index);
-		link_up = true;
+	// link up/down reporting
+	if (link_up && !(val & (1 << 31))) {
+		warning("Fabric LC5 link %u down", index);
+		link_up = 0;
+
+		// ratelimit
+		lib::udelay(1000000);
+	} else if (!link_up && (val & (1ULL << 31))) {
+		warning("Fabric LC5 link %u up", index);
+		link_up = 1;
+
+		// ratelimit
+		lib::udelay(1000000);
 	}
 }
 
