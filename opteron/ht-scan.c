9b19961609cd0df3b92d52c0603063c9ce491ef7
Daniel J Blueman <daniel@numascale.com>
2015-08-03 12:56:40 +0800

    Drop debug

--- opteron/ht-scan.c
+++ opteron/ht-scan.c
@@ -85,20 +85,6 @@ void Opteron::cht_print(const ht_t neigh, const link_t link)
 
 	if (ron < 11 || rtt >> 15)
 		warning("Ron %u is different than expected value of 11", ron);
-
-	val = 38 << 16; // Post1 -3dB deemphasis
-//	phy_write32(neigh, link, PHY_TX_DEEMPH + 5 * 8, 1, val);
-
-//	for (unsigned i = 0; i < 20; i++) {
-//		val = phy_read32(neigh, link, PHY_TX_DEEMPH + i * 8, 1);
-//		printf("phy%04X=%08x\n", PHY_TX_DEEMPH + i * 8, val);
-//	}
-
-	// verify phy value
-	for (unsigned i = 0; i < 19; i++)
-		// skip
-		if (i != 17)
-			printf("phy%04X=%08x\n", 0x400f + i * 0x80, phy_read32(neigh, link, 0x400f + i * 0x80, 1));
 }
 
 static bool proc_lessthan_b0(const ht_t ht)
@@ -224,11 +210,8 @@ void Opteron::ht_optimize_link(const ht_t nc, const ht_t neigh, const link_t lin
 				tempdata &= ~ 0xf;
 				tempdata |= adjtemp; // DllProcessFreqCtlIndex2
 
-	//			for (unsigned i = 0; i < 18; i++)
-//					phy_write32(neigh, link, 0x400f + i * 0x80, 1, tempdata);
 				phy_write32(neigh, link, PHY_RX_DLL_CTRL5_16, 1, tempdata); // Broadcast 16-bit
 			}
-
 		}
 
 		reboot = 1;
