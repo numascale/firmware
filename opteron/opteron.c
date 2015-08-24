447118d96fec5837ebbbc133f1c943bbf0eea3fe
Daniel J Blueman <daniel@numascale.com>
2015-08-24 16:17:56 +0800

    Allow use of WDT address field for other decode

--- opteron/opteron.c
+++ opteron/opteron.c
@@ -337,9 +337,8 @@ void Opteron::init(void)
 	val = read32(MC_NB_CTRL);
 	write32(MC_NB_CTRL, val | (1 << 12));
 
-	// make sure WDT MCA address reporting is enabled
-	val = read32(MC_NB_CONF_EXT);
-	write32(MC_NB_CONF_EXT, val | (1 << 24));
+	// Upon WDT, use address field for other decoding
+//	clear32(MC_NB_CONF_EXT, 1 << 24);
 
 	if (options->debug.northbridge)
 		disable_syncflood();
